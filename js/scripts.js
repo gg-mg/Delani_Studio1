856ea9854eebfffe113a6b4e2e319fc41c70a761
gg-mg <ggichuki2017@gmail.com>
2019-12-08 13:27:11 +0300

    On click Javasript added

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready( function() {
+    $("#design-image").click(function () {
+        $(this).hide();
+        $("#design").show();
+    });
+
+    $('#design').click(function(){
+        $(this).hide();
+        $('#design-image').show();
+    });
+});
+$(document).ready( function() {
+    $("#design-image").click(function () {
+        $(this).hide();
+        $("#development").show();
+    });
+
+    $('#development').click(function(){
+        $(this).hide();
+        $('#design-image').show();
+    });
+});
+$(document).ready( function() {
+    $("#design-image").click(function () {
+        $(this).hide();
+        $("#product-management").show();
+    });
+
+    $('#product-management').click(function(){
+        $(this).hide();
+        $('#design-image').show();
+    });
+});
