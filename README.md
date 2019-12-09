36c3181104afe064cd1140b6a67f97ead9197ddc
gg-mg <ggichuki2017@gmail.com>
2019-12-09 09:07:02 +0300

    Added Known Bugs on README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IP3
+
+## Known Bugs
+On deployment, the page sometimes displays ~ and IP3.
+Click on IP3 for the page to load.
--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
-$(document).ready( function() {
+$(document).ready(function () {
     $("#design-image").click(function () {
         $(this).hide();
         $("#design").show();
     });
 
-    $('#design').click(function(){
+    $('#design').click(function () {
         $(this).hide();
         $('#design-image').show();
     });
 });
-$(document).ready( function() {
+$(document).ready(function () {
     $("#design-image").click(function () {
         $(this).hide();
         $("#development").show();
     });
 
-    $('#development').click(function(){
+    $('#development').click(function () {
         $(this).hide();
         $('#design-image').show();
     });
 });
-$(document).ready( function() {
+$(document).ready(function () {
     $("#design-image").click(function () {
         $(this).hide();
         $("#product-management").show();
     });
 
-    $('#product-management').click(function(){
+    $('#product-management').click(function () {
         $(this).hide();
         $('#design-image').show();
     });
